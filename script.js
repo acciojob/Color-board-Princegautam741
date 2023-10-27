9f3f9e1a76e6a626dd5ed44fb24c816690e6b015
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-27 15:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container");
+  const squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      const originalColor = getComputedStyle(square).backgroundColor;
+      square.style.backgroundColor = getRandomColor();
+      setTimeout(() => {
+        square.style.transition = "background-color 1s";
+        square.style.backgroundColor = originalColor;
+      }, 1000);
+    });
+  });
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
