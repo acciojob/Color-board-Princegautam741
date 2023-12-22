aec4fa163c2c94e17f90a55789f069659e8adefe
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-22 17:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector(".container");
-  const squares = document.querySelectorAll(".square");
+	const container = document.querySelector(".container");
+	const squares = document.querySelectorAll(".square");
 
-  squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-      const originalColor = getComputedStyle(square).backgroundColor;
-      square.style.backgroundColor = getRandomColor();
-      setTimeout(() => {
-        square.style.transition = "background-color 1s";
-        square.style.backgroundColor = originalColor;
-      }, 1000);
-    });
-  });
+	squares.forEach((square) => {
+		square.addEventListener("mouseenter", () => {
+			const originalColor = getComputedStyle(square).backgroundColor;
+			square.style.backgroundColor = getRandomColor();
+			setTimeout(() => {
+				square.style.transition = "background-color 1s";
+				square.style.backgroundColor = originalColor;
+			}, 1000);
+		});
+	});
 
-  function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+	function getRandomColor() {
+		const letters = "0123456789ABCDEF";
+		let color = "#";
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
 });
